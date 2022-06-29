af22afc55796683fe64f0f506f7490697dfb0e0f
ABDULALI3468 <mrabdulali2003@gmail.com>
2022-06-29 07:14:43 -0700

    Created react basic app.

--- math-matician/src/App.js
+++ math-matician/src/App.js
@@ -6,8 +6,15 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Math Magician Single Page App using Javascript.</p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           Learn React
         </a>
       </header>
