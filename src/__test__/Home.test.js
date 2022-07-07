190726801d6239498a3b8c9feeb0facfa4ccea80
yohannesdagnachew <102184682+yohannesdagnachew@users.noreply.github.com>
2022-07-07 11:33:20 +0300

    Add test for Home

--- src/__test__/Home.test.js
+++ src/__test__/Home.test.js
@@ -1,1 +1,9 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import Home from "../pages/Home";
+import "@testing-library/jest-dom";
 
+it("Test the Home page", () => {
+  const home = renderer.create(<Home />).toJSON();
+  expect(home).toMatchSnapshot();
+});
